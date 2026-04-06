16a916295c0901fd08ce51882ded6a400653d971
t3ja5_pc <t3ja5.s1ngh@gmail.com>
2026-04-06 12:31:11 +0530

    4chanservice created. News aggragator created

--- 00conspire_backend/server.js
+++ 00conspire_backend/server.js
@@ -10,13 +10,29 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-	.then(() => console.log("✅ MongoDB Connected Successfully"))
-    .catch(err => console.error("❌ MongoDB Connection Error:", err));
+	.then(() => console.log("MongoDB Connected Successfully"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
 
 app.get('/',(req, res)=> {
 	res.send("News scrapper Api is running ...");
 });
 
 app.listen(PORT, ()=> {
-	console.log(`🚀 Server started on http://localhost:${PORT}`);
+	console.log(`Server started on http://localhost:${PORT}`);
+});
+
+////////////////////////////////////////////////////////////////////
+const {scrape4chan} = require('./services/4chanService');
+app.get('/scrape/:keyword' , async (req,res)=> {
+	const keyword = req.params.keyword;
+    try {
+        const itemsFound = await scrape4chan(keyword);
+        res.json({ 
+            message: "Scraping complete!", 
+            keyword: keyword,
+            new_items_saved: itemsFound 
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Scraping failed" });
+    }
 });
---
+++ 00conspire_backend/services/4chanService.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const news = require('../models/News');
+
+const scrape4chan = async (keyword) => {
+	try{
+		const response = await axios.get('https://a.4cdn.org/pol/catalog.json');
+		const pages =response.data;
+		let count =0;
+
+		for(let page of pages){
+			for(let thread of page.threads){
+
+				const title = thread.sub || "No Title";
+                		const body = thread.com || "";
+
+if (title.toLowerCase().includes(keyword.toLowerCase()) ||body.toLowerCase().includes(keyword.toLowerCase())){
+
+	const newsItem = {
+                        title: title,
+                        content: body.replace(/<[^>]*>?/gm, ''), // Strips HTML tags like <br>
+                        url: `https://boards.4channel.org/pol/thread/${thread.no}`,
+                        source: '4chan',
+                        keyword: keyword,
+                        status: 'unconfirmed' // Default for 4chan
+                    };
+
+	try {
+                        // 4. Save to MongoDB
+                        // .updateOne with upsert: true prevents duplicates based on URL
+                        await news.updateOne(
+                            { url: newsItem.url }, 
+                            { $set: newsItem }, 
+                            { upsert: true }
+                        );
+                        count++;
+                    	} catch (dbError) {}
+			}
+		}
+	}
+		return count;
+	}catch(error){
+		console.error("4chan Scrape Error:", error);
+        	throw error;
+		}
+};
+
+module.exports = {scrape4chan};
